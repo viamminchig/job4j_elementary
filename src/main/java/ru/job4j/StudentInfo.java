046c27368d6d4577f5f5ecef769a3eee4d0607e7
viamminchig <viamminchig@list.ru>
2023-11-30 16:59:14 +0700

    Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Vladimir Tailakov");
+        System.out.println("18.01.1980");
+    }
+}
